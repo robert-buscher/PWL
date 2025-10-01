aec637445907c52fb0602204eaade7685d18afdb
NicholasT205 <nicholas.tauro@student.fairfield.edu>
2025-10-01 11:20:21 -0400

    Update RemoveStopWords.java
    Almost finished, just needs to be tested

--- milestone-1/RemoveStopWords.java
+++ milestone-1/RemoveStopWords.java
@@ -16,14 +16,48 @@ public class RemoveStopWords
         scan.close();
         return stopWords;
     }
+    public static ArrayList<String> filterArticle(String articlePath, ArrayList<String> stopWords) throws Exception
+    {
+        ArrayList<String> filteredWords = new ArrayList<>();
+        Scanner scan = new Scanner(new File(articlePath));
+
+        while (scan.hasNext())
+        {
+            String word = scan.next().toLowerCase().replaceAll("[^a-z]", ""); // removes punctuation
+            if (!word.isEmpty() && !stopWords.contains(word)) 
+            {
+                filteredWords.add(word);
+            }
+        }
+        scan.close();
+        return filteredWords;
+    }
+    public static void saveCleanedArticle(String articlePath, ArrayList<String> filteredWords) throws Exception
+    {
+        String newFilePath = articlePath.replace(".txt", "_cleaned.txt"); // make new file to copy to
+
+        Files.write(Paths.get(newFilePath), filteredWords);
+
+        System.out.println("Preprocessed file: " + newFilePath);
+    }
     
     public static void main(String[] args)
     {
-        String stopWordsPath = "stopwords.txt";
-        ArrayList<String> stopWords = createStopWordsList(stopWordsPath);
+        try 
+        {
+            String stopWordsPath = "stopwords.txt";
+            ArrayList<String> stopWords = createStopWordsList(stopWordsPath);
+            String[] articlePaths = {topic2article1.txt}; // add file names here
 
-        String[] articles = {};
-
-        
+            for (String article : articlePaths) 
+            {
+                ArrayList<String> filtered = filterArticle(article, stopWords);
+                saveCleanedArticle(article, filtered);
+            }
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }

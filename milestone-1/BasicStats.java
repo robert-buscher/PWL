e48f8ccbdb8f84104a43a866faaa1e818d3aa860
robert-buscher <robert.buscher@student.fairfield.edu>
2025-10-01 11:35:26 -0400

    Update BasicStats.java

--- milestone-1/BasicStats.java
+++ milestone-1/BasicStats.java
@@ -1,1 +1,19 @@
+import java.io.*;
+import java.util.*;
 
+public class BasicStats
+{
+  Public static class(FileStats)
+  {
+    public final String fileName;
+    public final int totalWords;
+    public final int uniqueWords;
+    public FileStats(String fileName, int totalWords, int uniqueWords)
+      {
+      this.fileName = fileName;
+      this.totalWords = totalWords;
+      this.uniqueWords = uniqueWords;
+      }
+    }
+  
+}

2a440cfc6f14f66fde5962a31c399f56251ef24e
robert-buscher <robert.buscher@student.fairfield.edu>
2025-11-19 11:25:17 -0500

    Implement addNewArticle method for file copying

---
+++ milestone-3/AddNewArticle.java
@@ -0,0 +1,26 @@
+public static void addNewArticle(File topicFolder) 
+{
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter full path of new .txt article: ");
+    String newPath = sc.nextLine().trim();
+
+    File newArticle = new File(newPath);
+
+    if (!newArticle.exists()) 
+    {
+        System.out.println("File not found.");
+        return;
+    }
+
+    File dest = new File(topicFolder, newArticle.getName());
+    try 
+    {
+        Files.copy(newArticle.toPath(), dest.toPath());
+        System.out.println("Article added to topic: " + topicFolder.getName());
+    } 
+    catch (IOException e) 
+    {
+        System.out.println("Failed to copy file: " + e.getMessage());
+    }
+}
